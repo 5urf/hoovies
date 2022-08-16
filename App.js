d9381f67a7b144adfa8fa2c2cbd93b9a77d7f8e7
LeeYunHak <sza0203@naver.com>
2022-08-16 21:57:49 +0900

    #2.3 AppLoading part Three

--- App.js
+++ App.js
@@ -1,24 +1,15 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
+
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require("./1234.jpeg"));
-  };
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  const [assets] = useAssets([require("./1234.jpeg")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
   return <Text>hi loading done</Text>;
 }
