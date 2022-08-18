3a4d1060daa33f7b431ef722ef8628b80009e3e9
LeeYunHak <sza0203@naver.com>
2022-08-18 22:11:44 +0900

    #3.2 Movies Screen part One

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,27 +1,43 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { TouchableOpacity, Text } from "react-native";
 import { StyleSheet } from "react-native";
 import styled from "styled-components/native";
-
-const Btn = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+import Swiper from "react-native-swiper";
+import { Dimensions } from "react-native";
+const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
 `;
 
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
+const View = styled.View`
+  flex: 1;
 `;
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
-  navigation: { navigate },
-}) => (
-  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
-    <Title>Movies</Title>
-  </Btn>
-);
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const API_KEY = "b35d620b83e0b5a20867a4c2070d351c";
+
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const getNowPlaying = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+    );
+  };
+  return (
+    <Container>
+      <Swiper
+        loop={true}
+        autoplay={true}
+        autoplayTimeout={3.5}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+      </Swiper>
+    </Container>
+  );
+};
 
 const styles = StyleSheet.create({
   btn: {
