1a5c2ba7f30a12df39cff3ec8480bc7a5acf7bf2
LeeYunHak <sza0203@naver.com>
2022-08-18 21:23:10 +0900

    #2.14 Styled Components

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,14 +1,33 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
-const Movies = ({ navigation: { navigate } }) => {
-  return (
-    <TouchableOpacity
-      onPress={() => navigate("Stack", { screen: "Three" })}
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      <Text>Movies</Text>
-    </TouchableOpacity>
-  );
-};
+import { StyleSheet } from "react-native";
+import styled from "styled-components/native";
 
+const Btn = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: ${(props) => (props.selected ? "blue" : "red")};
+`;
+
+const Movies = ({ navigation: { navigate } }) => (
+  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
+    <Title selected={false}>Movies</Title>
+    <Title selected={true}>Movies</Title>
+  </Btn>
+);
+
+const styles = StyleSheet.create({
+  btn: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "blue",
+  },
+});
 export default Movies;
