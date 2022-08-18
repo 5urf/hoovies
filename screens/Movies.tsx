0c194472fa8f31c4b2ee8adc1166dc52861873bc
LeeYunHak <sza0203@naver.com>
2022-08-18 21:47:46 +0900

    #2.16 Typescript

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,3 +1,4 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
 import { StyleSheet } from "react-native";
@@ -14,7 +15,9 @@ const Title = styled.Text`
   color: ${(props) => props.theme.textColor};
 `;
 
-const Movies = ({ navigation: { navigate } }) => (
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
+  navigation: { navigate },
+}) => (
   <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
     <Title>Movies</Title>
   </Btn>
