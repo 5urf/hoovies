20e774ff86ee3d76dbdb52a2fa3146180e604c18
yhLee <sza0203@naver.com>
2022-08-23 17:39:15 +0900

    #3.20 Search part Three

--- components/Slide.tsx
+++ components/Slide.tsx
@@ -1,6 +1,13 @@
+import { useNavigation } from "@react-navigation/native";
 import { BlurView } from "expo-blur";
 import React from "react";
-import { StyleSheet, useColorScheme, View } from "react-native";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  useColorScheme,
+  View,
+} from "react-native";
 import styled from "styled-components/native";
 import { makeImgPath } from "../utils";
 import Poster from "./Poster";
@@ -49,27 +56,33 @@ const Slide: React.FC<SlidePorps> = ({
   voteAverage,
 }) => {
   const isDark = useColorScheme() === "dark";
+  const navigation = useNavigation();
+  const goToDetail = () => {
+    navigation.navigate("Stack", { screen: "Detail" });
+  };
   return (
-    <View style={{ flex: 1 }}>
-      <Bgimg
-        style={StyleSheet.absoluteFill}
-        source={{ uri: makeImgPath(backdropPath) }}
-      />
-      <BlurView
-        tint={isDark ? "dark" : "light"}
-        intensity={30}
-        style={StyleSheet.absoluteFill}
-      >
-        <Wrapper>
-          <Poster path={posterPath} />
-          <Column>
-            <Title>{originalTitle}</Title>
-            <Overview>{overview.slice(0, 90)}...</Overview>
-            {voteAverage > 0 ? <Votes>⭐️{voteAverage}/10</Votes> : null}
-          </Column>
-        </Wrapper>
-      </BlurView>
-    </View>
+    <TouchableWithoutFeedback onPress={goToDetail}>
+      <View style={{ flex: 1 }}>
+        <Bgimg
+          style={StyleSheet.absoluteFill}
+          source={{ uri: makeImgPath(backdropPath) }}
+        />
+        <BlurView
+          tint={isDark ? "dark" : "light"}
+          intensity={30}
+          style={StyleSheet.absoluteFill}
+        >
+          <Wrapper>
+            <Poster path={posterPath} />
+            <Column>
+              <Title>{originalTitle}</Title>
+              <Overview>{overview.slice(0, 90)}...</Overview>
+              {voteAverage > 0 ? <Votes>⭐️{voteAverage}/10</Votes> : null}
+            </Column>
+          </Wrapper>
+        </BlurView>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,23 +1,6 @@
-import react from "react";
+import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View, Text, TouchableOpacity } from "react-native";
-import { YELLOW_COLOR } from "../color";
-
-const ScreenOne = ({ navigation: { navigate } }) => (
-  <TouchableOpacity onPress={() => navigate("Two")}>
-    <Text>go to Two</Text>
-  </TouchableOpacity>
-);
-const ScreenTwo = ({ navigation: { navigate } }) => (
-  <TouchableOpacity onPress={() => navigate("Three")}>
-    <Text>go to Three</Text>
-  </TouchableOpacity>
-);
-const ScreenThree = ({ navigation: { navigate } }) => (
-  <TouchableOpacity onPress={() => navigate("Tabs", { screen: "Search" })}>
-    <Text>Go to Search</Text>
-  </TouchableOpacity>
-);
+import Detail from "../screens/Detail";
 
 const NativeStack = createNativeStackNavigator();
 
@@ -27,9 +10,7 @@ const Stack = () => (
       headerBackTitleVisible: false,
     }}
   >
-    <NativeStack.Screen name='One' component={ScreenOne} />
-    <NativeStack.Screen name='Two' component={ScreenTwo} />
-    <NativeStack.Screen name='Three' component={ScreenThree} />
+    <NativeStack.Screen name='Detail' component={Detail} />
   </NativeStack.Navigator>
 );
 
